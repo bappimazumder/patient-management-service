51d3aec7c48ba9bc1c36359fde41fedf8f41b258
Bappi Mazumder <mazumderbappi@gmail.com>
2024-12-12 19:07:57 +0600

    first commit

---
+++ src/test/java/com/bappi/patientmanagementservice/PatientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappi.patientmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
